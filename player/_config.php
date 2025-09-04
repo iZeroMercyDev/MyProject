79f232561d41a4832bf756cf0608fc73b366db7e
iZeroMercyDev <shantogmr1444@gmail.com>
2025-09-04 18:32:32 +0600

    Add files via upload

---
+++ player/_config.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+$websiteTitle = "AnimeZia"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = $websiteUrl . "https://cdnzia.pages.dev/images/logo.webp"; // Logo
+$contactEmail = ""; // Contact Email
+
+$version = "0.4";
+
+//Donate 
+$donate = "http://coindrop.to/animezia1";
+
+// Socials 
+$telegram = "https://t.me/Animezia_net"; // telegram
+$discord = ""; // Discord
+$redit = ""; // Reddit
+$twitter = ""; // Twitter
+ 
+
+
+$disqus = "https://animezia-net.disqus.com"; // Disqus
+
+
+// API URL
+$api = "https://gogo-api-topaz.vercel.app/"; // https://github.com/shashankktiwariii/anikatsu-api
+
+
+$banner = $websiteUrl . "/files/images/banner.png";  //Banner
+?>
